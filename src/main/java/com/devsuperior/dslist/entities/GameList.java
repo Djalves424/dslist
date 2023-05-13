a0ef6c1fb8e3bac015c85735c71e8788f6a5ec01
Diego Alves <djalves424@gmail.com>
2023-05-13 00:15:26 -0300

    SQL, projection, GET games by list

--- src/main/java/com/devsuperior/dslist/entities/GameList.java
+++ src/main/java/com/devsuperior/dslist/entities/GameList.java
@@ -7,7 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+	
 @Entity
 @Table(name = "tb_game_list")
 public class GameList {
